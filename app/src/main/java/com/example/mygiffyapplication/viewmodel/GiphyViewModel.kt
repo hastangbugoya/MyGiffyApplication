8c5b7f811e41861f1c0b1b3329101546bfde9389
Jeffrey Joseph <jjrzacal@outlook.com>
2023-03-06 20:32:55 -0800

    hmmmmm

--- app/src/main/java/com/example/mygiffyapplication/viewmodel/GiphyViewModel.kt
+++ app/src/main/java/com/example/mygiffyapplication/viewmodel/GiphyViewModel.kt
@@ -7,34 +7,32 @@ import androidx.lifecycle.viewModelScope
 import com.example.mygiffyapplication.data.Data
 import com.example.mygiffyapplication.network.MyRetrofit
 import com.example.mygiffyapplication.utility.Logger.jLog
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.*
 import java.io.IOException
 
 class GiphyViewModel : ViewModel() {
-    var trends : MutableLiveData<List<Data>> = MutableLiveData<List<Data>>().apply { value = listOf() }
+    var trends: MutableLiveData<List<Data>> =
+        MutableLiveData<List<Data>>().apply { value = listOf() }
 
-/*
-API only retrieves images rated G - even when tested from browser
-only the limit query field is functional
-*/
-    fun getAllTrends(limit : Int = 15) {
+    /*
+    API only retrieves images rated G - even when tested from browser
+    only the limit query field is functional
+    */
+    fun getAllTrends(limit: Int = 15) {
         viewModelScope.launch(Dispatchers.IO) {
-            withTimeoutOrNull(30000) {
                 try {
                     trends.postValue(
                         async {
                             MyRetrofit.getService().getTrends(limit = limit).body()?.data
                         }.await()
                     )
-                } catch (e : IOException) {
+                } catch (e: IOException) {
                     jLog(e.toString())
                     trends.value = listOf()
                 }
-            }
         }
     }
-    fun getTrendCount() : Int = trends.value?.size ?: 0
+
+    fun getTrendCount(): Int = trends.value?.size ?: 0
 }
+
