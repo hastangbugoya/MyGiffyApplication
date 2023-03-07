822a08f872f684621ad4bf47ae26a9c6bf9352d8
Jeffrey Joseph <jjrzacal@outlook.com>
2023-03-06 21:10:29 -0800

    hmmmmm

--- app/src/main/java/com/example/mygiffyapplication/viewmodel/GiphyViewModel.kt
+++ app/src/main/java/com/example/mygiffyapplication/viewmodel/GiphyViewModel.kt
@@ -20,19 +20,57 @@ class GiphyViewModel : ViewModel() {
     */
     fun getAllTrends(limit: Int = 15) {
         viewModelScope.launch(Dispatchers.IO) {
-                try {
-                    trends.postValue(
-                        async {
-                            MyRetrofit.getService().getTrends(limit = limit).body()?.data
-                        }.await()
-                    )
-                } catch (e: IOException) {
-                    jLog(e.toString())
-                    trends.value = listOf()
-                }
+            try {
+                trends.postValue(
+                    async {
+                        MyRetrofit.getService().getTrends(limit = limit).body()?.data
+                    }.await()
+                )
+            } catch (e: IOException) {
+                jLog(e.toString())
+                trends.value = listOf()
+            }
         }
     }
 
     fun getTrendCount(): Int = trends.value?.size ?: 0
 }
 
+
+
+
+//    fun getAllTrends(limit: Int = 15) {
+//        viewModelScope.launch(Dispatchers.Main) {
+//            trends.value = withTimeoutOrNull(30000) {
+//                try {
+//                    withContext(Dispatchers.IO) {
+//                        MyRetrofit.getService().getTrends(limit = limit).body()?.data
+//                    }
+//                } catch (e: IOException) {
+//                    jLog(e.toString())
+//                    listOf()
+//                }
+//            } ?: listOf<Data>().also {
+//                jLog("Timed out")
+//            }
+//        }
+//    }
+
+//    trends.postValue(
+//    try {
+//        withTimeoutOrNull(40000) {
+//            withContext(Dispatchers.IO) {
+//                MyRetrofit.getService().getTrends(limit = limit).body()?.data ?: listOf<Data>().also {
+//                    //API returned empty
+//                }
+//            }
+//        }.let {
+//            // Error timed out
+//            listOf()
+//        }
+//    } catch (e: Exception) {
+//        // Exception encountered
+//        listOf<Data>()
+//    }
+//    )
+//}
